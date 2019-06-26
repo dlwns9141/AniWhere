3497d1a82adee4abc021466e9af6dd756493625a
dlwns9141 <dlwns9141@gmail.com>
2019-06-26 11:02:43 +0900

    asd
    asd

--- AniProject/fe/src/components/core/Drawer.vue
+++ AniProject/fe/src/components/core/Drawer.vue
@@ -35,23 +35,7 @@
         </v-list-tile>
         <v-divider/>
         <v-menu offset-y>
-      <template v-slot:activator="{ on }">
-        <v-btn
-          color="primary"
-          dark
-          v-on="on"
-        >
-          Dropdown
-        </v-btn>
-      </template>
       <v-list>
-        <v-list-tile
-          v-for="(link, to) in links"
-          :key="index"
-          @click=""
-        >
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-        </v-list-tile>
       </v-list>
     </v-menu>
         <v-card>
